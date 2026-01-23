ee0304dd29e595dac9e7dbf25814ade14237050c
Alexander Hamilton <a.hamilton72006@gmail.com>
2026-01-22 17:50:24 -0800

    add cl -> clear alias (cuz it wasn't short enough)

--- O11-Vision/profile.ps1
+++ O11-Vision/profile.ps1
@@ -1,8 +1,11 @@
 Clear-Host
+
+# Aliases
+set-alias cl Clear-Host
 function activate { . .\venv\Scripts\activate } # quickly activates a venv script
 function venv { py @($args.Count ? $args : @()) -m venv venv; activate } # allows for venv 3.12 or venv 3.8 to specify version, but is optional
 
 # This "should" be the last line of my profile, but zoxide conflicts with it, so it is the second to last line in my profile.
-oh-my-posh init pwsh --config '%USERPROFILE%\GitHub\GalexY727\oh-my-posh-galexy\bubblesextra.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\alex\GitHub\GalexY727\oh-my-posh-galexy\bubblesextra.omp.json' | Invoke-Expression
 
 zoxide init powershell --cmd cd --hook prompt | Out-String | Invoke-Expression
